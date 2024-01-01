1946722557ee0ed164b42019f0542f952391360e
ShakibulDeveloper <epiktheme@gmail.com>
2024-01-02 00:27:16 +0600

    first commit

---
+++ lib/presentation/ui/screens/login_screen.dart
@@ -0,0 +1,56 @@
+import 'package:crafty_bay/presentation/ui/widgets/logo.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 120),
+                const Logo(width: 85),
+                const SizedBox(height: 16),
+                Text(
+                  "Welcome Back",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                Text(
+                  "Please Inter Your Email Address",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  height: 45,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Email Address",
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Next"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:crafty_bay/presentation/ui/screens/login_screen.dart';
+import 'package:crafty_bay/presentation/ui/widgets/logo.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            const Logo(width: 115),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const SizedBox(height: 28),
+            Text(
+              "Version 1.0",
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    splashScreenTimeOut();
+  }
+
+  Future<void> splashScreenTimeOut() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.offAll(const LoginScreen());
+  }
+}
